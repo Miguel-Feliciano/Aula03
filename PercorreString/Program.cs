e3837b4ee88f08f802978233263965c523f95733
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2020-03-26 10:11:19 +0000

    Percorrestring

---
+++ PercorreString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declarar variáveis
+            string str, c;
+            //pedir string ao user 
+            Console.Write("Insira uma string: ");
+            str = Console.ReadLine();
+            // pedir carater c ao user
+            Console.Write("Insira uma string: ");
+            c = Console.ReadLine();
+            // percorrer string
+            foreach (char cDaStr in str);
+            {
+                // imprimir carater da string, exceto se for carater c
+            if (cDaStr != c[0])
+            {
+                Console.Write(cDaStr);
+            }
+            
+            }
+            Console.WriteLine();
+    
+        
+        }       
+    }
+}
